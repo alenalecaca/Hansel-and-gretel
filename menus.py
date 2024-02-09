28a42df9a65cb4cc238ea2c8f0415aabc2c767f0
khayo226 <153922039+khayo226@users.noreply.github.com>
2024-02-09 14:42:41 +0100

    Update menus.py
    update: mis à jour le code avec l'addition du code du jeu principal, mais il manque les textures du jeu principal, notamment du personnage

--- menus.py
+++ menus.py
@@ -1,6 +1,6 @@
 # Pyxel Studio
 
-import pyxel
+import pyxel, random
 
 # initialisation à remplacer par celles du projet final
 pyxel.init(128, 128)                #initialidstaion ecran 
@@ -24,17 +24,28 @@ menu_touches=[0,1,2,3]              #menu des fonction des touches courantes
 # parametrages des tailles de touches: constantes lisibles mais non modifiables par les fonctions
 # tout marche si on change la taille des touches ou d'autre paramétres
 # mais si on change la taille des touches, il faut les redessiner dans le fichier pyxres
+ingame = False
+
 lg_touche = 28                      # longueur d une touche 
 ht_touche = 11                      # hauteur d'une touche
 separ = 2                           # taille separateur entre touches
 x_menu = 5                          # place en X du menu
 y_menu = 90                         # place en Y du menu
 
+perso_x = 60
+perso_y = 60
+sol = 60
+hauteur_perso = 8
+saut = False
+
+blocs_liste = []
+
 ########################################################################
 ############### FONCTIONS LOCALES ######################################
 ########################################################################
 # zone_touche: calcule les intervalles de zone sensible d'une touche
 # dit sur le point x,y est dans la zone de la touche num_touche
+
 def zone_touche(num_touche,x,y):     #
     x_min = x_menu + num_touche*(lg_touche+separ)
     x_max = x_min + lg_touche
@@ -44,7 +55,7 @@ def zone_touche(num_touche,x,y):     #
         return(True)
     else:
         return(False)
-    
+        
 # fct_touche: dit si des coordonnées x_clic, y_click sont dans une zone sensible d'une touche,
 # renvoie le code de la fonction de la touche si vrai, 255 si faux
 # sera appelé à chaque click gauche de souris (dans "update")
@@ -65,6 +76,42 @@ def affiche_menu(menu):             # affiche les touches de "menu" (tableau)
         # de taille lg_touche sur ht_touche
         pyxel.blt(x_menu+ind*(lg_touche+separ),y_menu,0,0,ht_touche*menu[ind],lg_touche,ht_touche)
 
+
+###### de app.py ###########
+def blocs_creation(blocs_liste):
+    """création aléatoire de blocs"""
+
+    # un bloc par seconde
+    if (pyxel.frame_count % 30 == 0):
+        blocs_liste.append([120, random.randint(0, 120)])
+    return blocs_liste
+
+def blocs_deplacement(blocs_liste):
+    """déplacement des blocs vers la gauche et suppression s'ils sortent du cadre"""
+    """mouvement avec les touches de directions"""
+#variables blocs provisoires, juste pour montrer l'effet de scrolling    
+    for bloc in blocs_liste:
+        bloc[0] -= 1
+        if  bloc[0]<0:
+            blocs_liste.remove(bloc)
+    return blocs_liste
+
+def perso_deplacement():
+    global perso_y, sol, saut
+
+    if pyxel.btnp(pyxel.KEY_UP) and perso_y == sol:
+        saut = True
+    if saut:
+        perso_y -=3
+        if perso_y <= 40:
+            saut = False
+    else:
+        perso_y +=3
+        if perso_y >= sol:
+            perso_y = sol
+    
+
+
 #==================================================================
 # fonction à appeler dans la fonction "update" du module principal
 #==================================================================
@@ -72,6 +119,7 @@ def menus_update():
     # GESTION DES MENUS
     # si click gauche souris, tester si sur une touche fonction
     global menu_touches
+    global ingame
     if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
         fonction = fct_touche(pyxel.mouse_x,pyxel.mouse_y)
         print("valeur foncton =",fonction)    #### pour DEBUG: à enlever
@@ -82,7 +130,7 @@ def menus_update():
             pyxel.quit()
         elif fonction == 1:
             # PLAY
-            pass                    # a remplacer lancer le jeu
+            ingame = True                   # a remplacer lancer le jeu
         elif fonction == 2:
             # OPTIONS
             menu_touches=[4,5,6,7]  # menu options
@@ -118,7 +166,7 @@ def menu_init():
     global separ                        # taille separateur entre touches
     global x_menu                       # place en X du menu
     global y_menu                       # place en Y du menu
-    
+    global ingame
     pyxel.mouse(True)                   # rendre le curseur visible
 
     # parametrages des zones et initialisations des variables globales
@@ -137,14 +185,13 @@ def menu_init():
     separ = 2                           # taille separateur entre touches
     x_menu = 5                          # place en X du menu
     y_menu = 90                         # place en Y du menu
-
-
+    ingame = False
 #================================================================
 # fonction à appeler dans la fonction "draw" du module principal
 #================================================================
 def menu_draw():
-    affiche_menu(menu_touches)
-
+   affiche_menu(menu_touches) 
+        
 
 
 ####################################################################
@@ -152,21 +199,56 @@ def menu_draw():
 ####################################################################
         
 def update():                       #fonction de calcul periodique
-   
+    global blocs_liste
+
     # GESTION DES MENUS
     # appel de la fonction "update" du menu: à reporter dans module main
-    menus_update()
-    
+    if not ingame:
+        menus_update()
+    else:
+        
+        # creation des blocs
+        blocs_liste = blocs_creation(blocs_liste)
 
+            # mise a jour des positions des blocs
+        blocs_liste = blocs_deplacement(blocs_liste)
 
-def draw():                         # fonction d'affichage periodique
-    pyxel.cls(0)                    # effacement ecran
-    pyxel.rect(10, 10, 20, 20, 11)
-    pyxel.rect(20,20,80,20,12)
-    pyxel.text(30, 27, "Hansel & Gretel", 8 )
-    
+        perso_deplacement()
+            
+def draw(): 
+    if not ingame:                      # fonction d'affichage periodique
+        pyxel.cls(0)                    # effacement ecran
+        pyxel.rect(10, 10, 20, 20, 11)
+        pyxel.rect(20,20,80,20,12)
+        pyxel.text(30, 27, "Hansel & Gretel", 8 )
+     
     # appel de la fonction "draw" du menu: à reporter dans module main
-    menu_draw()
+        menu_draw()
+    else:
+        
+        # vide la fenetre
+        pyxel.cls(0)
+
+        # blocs
+        for bloc in blocs_liste:
+            pyxel.rect(bloc[0], bloc[1], 8, 8, 8)
+
+
+        if pyxel.btn(pyxel.KEY_DOWN):
+            if pyxel.frame_count % 15 < 5:
+                pyxel.blt(perso_x, perso_y, 0, 14, 16, 16, 16, 2)
+            elif pyxel.frame_count % 15 >= 5 and pyxel.frame_count % 15 < 10:
+                pyxel.blt(perso_x, perso_y, 0, 14, 48, 16, 16, 2)
+            else:
+                pyxel.blt(perso_x, perso_y, 0, 14, 32, 16, 16, 2)
+        else:  
+            if pyxel.frame_count % 15 < 5:
+                pyxel.blt(perso_x, perso_y, 0, 0, 48, 13, 16, 2)
+            elif pyxel.frame_count % 15 >= 5 and pyxel.frame_count % 15 < 10:
+                pyxel.blt(perso_x, perso_y, 0, 0, 32, 13, 16, 2)
+            else:
+                pyxel.blt(perso_x, perso_y, 0, 0, 64, 13, 16, 2)
+
 
 # appel de la fonction d'initialisation des parametres de menu
 # a faire depuis module main si le code du corps du module "menu" n'est pas execute
